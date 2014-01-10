23c28815fb4f948bb684dd1a8f935afff73f260a
Grzegorz Różycki <grozycki@op.pl>
2014-01-10 20:05:57 +0100

    physics update

--- physics.js
+++ physics.js
@@ -1,12 +1,37 @@
 var PHYSICS = (function () {
     'use strict';
     var API = Object.create(null),
-        snake = Object.create(null),
         movement = null,
-        fruit = [];
-    // public API
+        fruit = [],
+        snake = [];
+    // private methods:
+    function checkForColisions() {}
+    function fruitCollected() {}
+    function moveSnake() {
+        switch (movement) {
+            case 'n':
+                break;
+            case 's':
+                break;
+            case 'w':
+                break;
+            case 'e':
+                snake = [sanke[0][0] + 1, snake[0][1]];
+                break;
+            default:
+                throw new Error('PHYSICS: unknown movement direction: ' + movement);
+        }
+    }
+    // public API:
     API.init = function () {
-
+        var i = 0;
+        movement = 'e';
+        for (; i < CONF.initLength; i += 1) {
+            snake.push(CONF.initPosition);
+        }
+        fruit = [10, 10];
+        console.log(snake);
+        console.log(fruit);
     };
     API.step = function () {
         var tS = Date.now(),
@@ -17,10 +42,14 @@ var PHYSICS = (function () {
                 Math.pow(Math.sin(i), j);
             }
         }
+        moveSnake();
         console.log('Step took: ' + (Date.now() - tS) + '[ms]');
     };
     API.getSnake = function () {
         return snake;
+    };
+    API.getFruit = function () {
+        return fruit;
     };
     return Object.freeze(API);
 } ());
