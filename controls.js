5d7548ac39efd790d65d93f7dfde8a05f61feaed
Grzegorz Różycki <grozycki@op.pl>
2014-01-22 18:00:57 +0100

    added function keys and action keys separation: actions [in game, e.g. move left, fire, etc.] functions are about game controlling [pause, play, exit

--- controls.js
+++ controls.js
@@ -1,21 +1,37 @@
 var CONTROLS = function () {
     'use strict';
     // private members:
-    var actions = null,   // posible actions in game
-        bindings = null;  // key boundend with actions
+    var functionBindings = null,
+        actionBindings = null,  // key boundend with actions
+        functions = null, // functions e.g. pause/resume
+        actions = null;   // posible actions in game
 
+
+    function functionKeyPress(keyCode) {
+        var b = functionBindings[keyCode];
+        if (functions.hasOwnProperty(b)) {
+            if (typeof functions[b] === 'function') {
+                functions[b]();
+            }
+        }
+    }
     // public members:
     return Object.freeze({
         keyDownEventHandler: function (e) {
-            var b = bindings[e.keyCode];
+            var b = actionBindings[e.keyCode];
             if (actions.hasOwnProperty(b)) {
+                // if action key, make action checked
                 actions[b] = true;
             }
         },
         keyUpEventHandler: function (e) {
-            var b = bindings[e.keyCode];
+            var b = actionBindings[e.keyCode];
             if (actions.hasOwnProperty(b)) {
+                // if action key, make action unchecked
                 actions[b] = false;
+            } else {
+                // maybe it's a function key?
+                functionKeyPress(e.keyCode);
             }
         },
         getActions: function () {
@@ -23,30 +39,35 @@ var CONTROLS = function () {
         },
         init: function () {
             if (CANVAS) {
-                bindings = {};
+                functionBindings = {};
+                actionBindings = {};
+                functions = {
+                    "pause": GAME.toggle    // action name: action handler
+                };
                 actions = {
                     "n" : false,    // move north
                     "s" : false,    //      south
                     "w" : false,    //      west
                     "e" : false,    //      east
                     "p" : false     // pause game
-                }
+                };
                 // append listeners:
                 CANVAS.addEventListener("keydown", CONTROLS.keyDownEventHandler);
                 CANVAS.addEventListener("keyup", CONTROLS.keyUpEventHandler);
                 CANVAS.addEventListener("click", GAME.toggle);
                 // standard key setup / bindings:
-                CONTROLS.bindKey(65, "w");  // a key
-                CONTROLS.bindKey(68, "e");  // d key
-                CONTROLS.bindKey(87, "n");  // w key
-                CONTROLS.bindKey(83, "s");  // s key
-                CONTROLS.bindKey(37, "w");  // left-arrow
-                CONTROLS.bindKey(39, "e");  // right arrow
-                CONTROLS.bindKey(38, "n");  // up arrow
-                CONTROLS.bindKey(40, "s");  // down arrow
-                CONTROLS.bindKey(80, "p");  // p key
-                CONTROLS.bindKey(40, "s");  // down arrow
-                //CONTROLS.bindKey(27, "s");  // ESC key [doesn't work...]
+                CONTROLS.bindActionKey(65, "w");  // a key
+                CONTROLS.bindActionKey(68, "e");  // d key
+                CONTROLS.bindActionKey(87, "n");  // w key
+                CONTROLS.bindActionKey(83, "s");  // s key
+                CONTROLS.bindActionKey(37, "w");  // left-arrow
+                CONTROLS.bindActionKey(39, "e");  // right arrow
+                CONTROLS.bindActionKey(38, "n");  // up arrow
+                CONTROLS.bindActionKey(40, "s");  // down arrow
+                //CONTROLS.bindActionKey(80, "p");  // p key
+                CONTROLS.bindActionKey(40, "s");  // down arrow
+                //CONTROLS.bindActionKey(27, "s");  // ESC key [doesn't work...]
+                CONTROLS.bindFunctionKey(80, 'pause');
                 // everything went OK
                 // tell gameManager, that i'm ready
             }
@@ -64,14 +85,25 @@ var CONTROLS = function () {
                     }
                 }
                 // clear bindings
-                bindings = {}; // aż tak drastycznie? nazwa mówi zatrzymaj, nie zniszcz...
+                functionBindings = {}; // aż tak drastycznie? nazwa mówi zatrzymaj, nie zniszcz...
+                actionBindings = {}; // aż tak drastycznie? nazwa mówi zatrzymaj, nie zniszcz...
             }
         },
-        bindKey: function (key, action) {
+        bindFunctionKey: function (key, action) {
+            key = +key;
+            action = (functions.hasOwnProperty(action)) ? action : null;
+            if (action !== null) {
+                functionBindings[key] = action;
+                return true;
+            } else {
+                return false;
+            }
+        },
+        bindActionKey: function (key, action) {
             key = +key;
             action = (actions.hasOwnProperty(action)) ? action : null;
             if (action !== null) {
-                bindings[key] = action;
+                actionBindings[key] = action;
                 return true;
             } else {
                 return false;
